45bc35922e25f8d2c674a887aa49d3fa0fdee111
Aristidis Dinakus <arisntin1@gmail.com>
2020-10-16 18:07:26 +0300

    Destroy after finding cpe's

--- GUI.py
+++ GUI.py
@@ -1,12 +1,53 @@
+import threading
 from tkinter import *
 from tkinter import messagebox
-from main import TEST
 from PIL import Image,ImageTk
+import json
+import nmap
+import mysql.connector
+from JsonParsing import NmapParse
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="root",
+    database="cpeiotdb"
+)
 
 
+mycursor = mydb.cursor()
 
+class Procedures:
+        def procedure(ipaddress):
+                ipaddrs = ipaddress
+                np = NmapParse()
+                values = np.NmapScanParse(ipaddrs)
+                val = '0'
 
-
+                for i in values:
+                        cpevl = (i)
+                        cpevl2 = str(cpevl).replace('[', '(').replace(']', ',)')
+                        cpevl3 = cpevl2.replace('(\'', '').replace('\',)', '').replace(" ", '')
+                        query = "SELECT cpe FROM cpetbl WHERE cpe LIKE '{}'".format(cpevl3)
+                        mycursor.execute(query, cpevl2)
+                        myresult = mycursor.fetchall()
+                        if len(myresult) == 0:
+                                print("Device CPE " + cpevl2 + "  NOT IN table")
+                                cve, description = np.ParseNVDJson(cpevl3)
+                                print(cve, description)
+                        else:
+                                query2 = "SELECT cpe FROM cpetbl WHERE cpe LIKE '{}' AND iot = 'YES'".format(cpevl3)
+                                mycursor.execute(query2, cpevl2)
+                                myresult2 = mycursor.fetchall()
+                                if len(myresult2) == 0:
+                                        print("Device CPE " + cpevl2 + " already exists in table and IS NOT iot")
+                                        np.ParseNVDJson(cpevl3)
+                                        # c11 = CPE((cpevl3), CPE.VERSION_2_2)
+                                        # c22 = c11.as_wfn()
+                                        # print("CHECK HERE MAN :",c22)
+                                else:
+                                        print("Device CPE " + cpevl2 + " already exists in table and IS iot")
+                UI.close_window(root)
 class UI:
 
         def __init__(self,root):
@@ -14,6 +55,7 @@ class UI:
                 self.root.title("IOT VULNERABILITY SYSTEM")
                 self.root.geometry("1600x900+0+0")
                 self.root.config(bg="white")
+
 
 
         # Background
@@ -32,11 +74,18 @@ class UI:
                 self.btn_img=ImageTk.PhotoImage(file="images/magnifying-glass.png")
                 btn=Button(frame1,image=self.btn_img,bg="blue",cursor="hand2",command=self.provide_address).place(x=1010, y=25)
 
+        def close_window(self):
+                root.destroy()
+
         def provide_address(self):
                 if self.var_ipentry.get()=="":
                         messagebox.showerror("Error! Provide Value","NETWORK ADDRESS CANNOT BE BLANK",parent=self.root)
                 else:
-                        TEST.procedure(self.var_ipentry.get())
+                        threading.Thread(target = Procedures.procedure(self.var_ipentry.get())).start()
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -6,23 +6,6 @@ from tkinter import *
 
 
 
-
-
-from cpe import CPE
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="root",
-    database="cpeiotdb"
-)
-
-
-
-
-
-
-mycursor = mydb.cursor()
-
 #mycursor.execute("DELETE from cpetbl")
 class TEST:
 #print(mydb)
@@ -31,36 +14,6 @@ class TEST:
         pass
 
 
-    def procedure(ipaddress):
-        ipaddrs = ipaddress
-        np = NmapParse()
-        values = np.NmapScanParse(ipaddrs)
-        val = '0'
-
-
-        for i in values:
-                cpevl = (i)
-                cpevl2 = str(cpevl).replace('[', '(').replace(']', ',)')
-                cpevl3 = cpevl2.replace('(\'', '').replace('\',)', '').replace(" ", '' )
-                query = "SELECT cpe FROM cpetbl WHERE cpe LIKE '{}'".format(cpevl3)
-                mycursor.execute(query, cpevl2)
-                myresult = mycursor.fetchall()
-                if len(myresult) == 0:
-                    print("Device CPE " + cpevl2 + "  NOT IN table")
-                    cve, description = np.ParseNVDJson(cpevl3)
-                    print (cve, description)
-                else:
-                    query2 = "SELECT cpe FROM cpetbl WHERE cpe LIKE '{}' AND iot = 'YES'".format(cpevl3)
-                    mycursor.execute(query2, cpevl2)
-                    myresult2 = mycursor.fetchall()
-                    if len(myresult2) == 0:
-                        print("Device CPE " + cpevl2 + " already exists in table and IS NOT iot")
-                        np.ParseNVDJson(cpevl3)
-                        #c11 = CPE((cpevl3), CPE.VERSION_2_2)
-                        #c22 = c11.as_wfn()
-                        # print("CHECK HERE MAN :",c22)
-                    else:
-                        print("Device CPE " + cpevl2 + " already exists in table and IS iot")
 
 
 
@@ -83,4 +36,4 @@ class TEST:
 #print (values)
 #print(mycursor.rowcount, "record were inserted.")
 #mycursor.execute("CREATE TABLE cpetbl (id INT AUTO_INCREMENT PRIMARY KEY, cpe VARCHAR(255) UNIQUE, iot VARCHAR(255))")
-mydb.commit()
+#mydb.commit()
